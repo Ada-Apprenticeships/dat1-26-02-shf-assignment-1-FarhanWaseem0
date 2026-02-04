da6fd6ff143b5c46d1c35f7fe48c08df09b91e9a
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-02-04 16:48:12 +0000

    V2

--- src/insertion.sql
+++ src/insertion.sql
@@ -1,2 +1,6 @@
 .open fittrackpro.db
-.mode column
+.mode box
+
+INSERT INTO locations (name, address, phone_number, email, opening_hours)
+VALUES  ('Downtown Fitness', '123 Main St', 'Cityville', '555-1234', 'downtown@fitnesstrackpro.com', '6:00-22:00')
+
