53f5c882f453b4e6dd5ab6e921a542c82c7e2bb7
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-03-02 07:36:32 +0000

    V24

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -1,26 +1,32 @@
+.open fittrackpro.db
+PRAGMA foreign_keys = ON;
+.mode box
+
 .read schema.sql
 .read insertion.sql
-.open fittrackpro.db
-.mode column
 
 -- 1.1
+.print
 SELECT member_id, first_name, last_name, email, join_date
 FROM members;
 
 -- 1.2
+.print
 UPDATE members
 SET phone_number = '07000 100005', email = 'emily.jones.updated@email.com'
 WHERE member_id = 5;
 
 -- 1.3
+.print
 SELECT COUNT(*) AS Members_Total
 FROM members;
 
 -- 1.4
+.print
 SELECT member_id, COUNT(*) AS Classes_Registered
 FROM class_attendance 
-GROUP member_id
-ORDER Classes_Registration DESC
+GROUP BY member_id
+ORDER BY COUNT(*) DESC
 LIMIT 1;
 
 -- 1.5
