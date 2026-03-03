5fa1c0180209611dc8cd353087664dbeba9448b3
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-03-03 22:57:17 +0000

    V41

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -1,10 +1,25 @@
 .open fittrackpro.db
-.mode column
+PRAGMA foreign_keys = ON;
+.mode box
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/schema.sql
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/insertion.sql
 
 -- 5.1 
+SELECT members.member_id,
+members.first_name,
+members.last_name,
+memberships.type AS membership_type,
+members.join_date
+FROM memberships
+JOIN members
+ON memberships.member_id = members.member_id
+WHERE memberships.status = 'Active';
 
 
 -- 5.2 
+SELECT 
+
+
 
 
 -- 5.3 
