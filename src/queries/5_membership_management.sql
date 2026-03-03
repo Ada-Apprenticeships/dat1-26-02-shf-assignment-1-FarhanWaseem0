888fdcc7c98f1a9cc0395bd15fbef9e582949047
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-03-03 23:03:51 +0000

    V42

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -17,10 +17,23 @@ WHERE memberships.status = 'Active';
 
 
 -- 5.2 
-SELECT 
-
-
+SELECT memberships.type AS membership_type,
+AVG(
+    (julianday(attendance.check_out_time) - julianday(attendance.check_in_time)) * 1440) AS avg_visit_duration_minutes
+FROM attendance 
+JOIN memberships
+    ON attendance.member_id = memberships.member_id
+GROUP BY memberships.type;
 
 
 -- 5.3 
+SELECT members.member_id,
+    members.first_name,
+    members.last_name,
+    members.email,
+    memberships.end_date
+FROM memberships
+JOIN members
+ON memberships.member_id = members.member_id
+WHERE memberships.end_date BETWEEN '2025-01-01' AND '2025-12-31';
 
