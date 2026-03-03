6ced96faf5df50b8e1604db0097c87649ddb1c8d
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-03-03 00:29:20 +0000

    V35

--- src/queries/4_class_scheduling.sql
+++ src/queries/4_class_scheduling.sql
@@ -1,5 +1,8 @@
 .open fittrackpro.db
-.mode column
+PRAGMA foreign_keys = ON;
+.mode box
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/schema.sql
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/insertion.sql
 
 -- 4.1 
 
@@ -8,10 +11,12 @@
 
 
 -- 4.3 
-
+INSERT INTO class_attendance(class_id, schedule_id, member_id, attendance_status)
+VALUES (1, 1, 11, 'Registered');
 
 -- 4.4 
-
+DELETE FROM class_attendance
+WHERE schedule_id = 7 AND member_id = 3;
 
 -- 4.5 
 
