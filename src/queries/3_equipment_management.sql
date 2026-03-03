946c89dca8044d3e721fd956e10088d243de6a86
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-03-03 00:10:55 +0000

    V33

--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -1,11 +1,20 @@
 .open fittrackpro.db
-.mode column
+PRAGMA foreign_keys = ON;
+.mode box
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/schema.sql
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/insertion.sql
 
 -- 3.1 
-
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date >= '2025-01-01'
+AND next_maintenance_date <= '2025-01-31';
 
 -- 3.2 
-
+SELECT type AS equipment_type,
+COUNT(*) AS count
+FROM equipment
+GROUP BY type;
 
 -- 3.3 
 
