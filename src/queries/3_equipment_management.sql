f8f39679fc4c1a38f1a534137d4db7626b71e698
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-03-04 11:59:53 +0000

    V49

--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -1,8 +1,6 @@
 .open fittrackpro.db
 PRAGMA foreign_keys = ON;
 .mode box
-.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/schema.sql
-.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/insertion.sql
 
 
 -- 3.1 
--- src/queries/8_personal_training.sql
+++ src/queries/8_personal_training.sql
@@ -1,8 +1,7 @@
 .open fittrackpro.db
 PRAGMA foreign_keys = ON;
 .mode box
-.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/schema.sql
-.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/insertion.sql
+
 
 -- 8.1 
 -- Retrieve all personal training sessions delivered by Ivy Irwin.
