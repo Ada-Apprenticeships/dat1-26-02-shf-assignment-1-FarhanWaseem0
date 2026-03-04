acea78670305d368a5132567ebfdd3d992378062
FarhanWaseem0 <farhan.waseem@ada.ac.uk>
2026-03-04 12:09:33 +0000

    V50

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -1,6 +1,8 @@
 .open fittrackpro.db
 PRAGMA foreign_keys = ON;
 .mode box
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/schema.sql
+.read /workspaces/dat1-26-02-shf-assignment-1-FarhanWaseem0/src/insertion.sql
 
 
 -- 5.1 
